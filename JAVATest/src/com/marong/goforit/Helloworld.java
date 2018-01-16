51792ee5d327e405f92c84d069239adc69f52215
marong <maronggoforit@163.com>
2018-01-16 18:25:36 +0800

    update 02

--- JAVATest/src/com/marong/goforit/Helloworld.java
+++ JAVATest/src/com/marong/goforit/Helloworld.java
@@ -3,7 +3,8 @@ package com.marong.goforit;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.print("Hello world");                          
+		System.out.print("Hello world");   
+		System.out.print("Hello world");   
 	}
 
 }
